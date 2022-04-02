37670b56dde945b2421c72c723fed22cf62ecd70
Glauber Holanda <glauberholanda10@gmail.com>
2022-04-02 10:55:15 -0300

    refactor(hook): add product to cart

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -19,24 +19,51 @@ interface CartContextData {
   updateProductAmount: ({ productId, amount }: UpdateProductAmount) => void;
 }
 
+const LocalStorageCartKey = '@RocketShoes:cart';
+
 const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem(LocalStorageCartKey);
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const productExists = cart.find(product => product.id === productId);
+
+      if(productExists) {
+        const { data: productStock } = await api.get<Stock>(`/stock/${productId}`);
+        const newAmount = productExists.amount + 1;
+
+        if(newAmount > productStock.amount) {
+          toast.error('Quantidade solicitada fora de estoque');
+          return;
+        }
+
+        productExists.amount = newAmount;
+        const updatedCart = cart.map(product => (product.id === productId ? productExists : product));
+        setCart(updatedCart);
+        localStorage.setItem(LocalStorageCartKey, JSON.stringify(updatedCart));
+        return;
+      }
+
+      const { data: newProductWithoutAmount } = await api.get<Product>(`/products/${productId}`);
+      const newProductWithAmount = { ...newProductWithoutAmount, amount: 1 };
+      const updatedCart = [...cart, newProductWithAmount];
+      setCart(updatedCart);
+      localStorage.setItem(LocalStorageCartKey, JSON.stringify(updatedCart));
+
+      return;
+
     } catch {
-      // TODO
+      toast.error('Erro na adição do produto');
     }
   };
 
